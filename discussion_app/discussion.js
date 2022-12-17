25da94b84c93299c699645bcc9a4e64e14452a23
Mohammed Huzefa <huzefamohammed10@gmail.com>
2022-12-17 15:11:52 +0530

    ok

--- discussion_app/discussion.js
+++ discussion_app/discussion.js
@@ -19,6 +19,12 @@ var commentatorNameNode = document.getElementById('pickName')
 var commentNode = document.getElementById('pickComment')
 var submitCommentNode = document.getElementById('commentBtn')
 
+var questionSearchNode = document.getElementById('questionSearch');
+var upvote = document.getElementById('upvote');
+var downvote = document.getElementById('downvote');
+
+var newQuestionFormBtn = document.getElementById('newQuestionForm');
+
 // function which is call when page is open or refresh
 function onLoad(){
 
@@ -40,7 +46,9 @@ function onQuestionSubmit(){
     var question = {
         title : questionTitleNode.value,
         description : questionDescriptionNode.value,
-        responses : []
+        responses : [],
+        upvotes: 0,
+        downvotes: 0
     }
     addQuestionInPanel(question);
     saveQuestion(question);
@@ -60,6 +68,7 @@ function saveQuestion(question){
 //append question to the left panel 
 function addQuestionInPanel(question){
     var questionContainerNode = document.createElement('div');
+    questionContainerNode.setAttribute('id', question.title);
     questionContainerNode.style.margin = '1vh 0';
     questionContainerNode.style.cursor = 'pointer'
 
@@ -70,6 +79,14 @@ function addQuestionInPanel(question){
     var newQuestionDescriptionNode = document.createElement('p');
     newQuestionDescriptionNode.innerHTML = question.description;
     questionContainerNode.appendChild(newQuestionDescriptionNode);
+
+    var upvoteTextNode = document.createElement('h5');
+    upvoteTextNode.innerHTML = 'Upvote = '+ question.upvotes;
+    questionContainerNode.appendChild(upvoteTextNode);
+
+    var downvoteTextNode = document.createElement('h5');
+    downvoteTextNode.innerHTML = 'Downvote = '+ question.downvotes;
+    questionContainerNode.appendChild(downvoteTextNode);
 
     allQuestionsListNode.appendChild(questionContainerNode);
 
@@ -112,29 +129,51 @@ function onQuestionClick(question){
         hideQuestionPanel();
         // show the options which is visible when we clicked on question
         showDetailsOfQuestion(question);
+        // clear question details
+        clearQuestionDetailes();
+        // clear response details
+        clearResponsesDetailes();
         // show the clicked question
         addQuestionInRight(question);
+        // show previouse responses
+        question.responses.forEach(function(response){
+            addResponseInPanel(response);
+        });
 
         // listen the comment button
-        submitCommentNode.addEventListener('click', onResponseSubmit(question));
+        submitCommentNode.onclick = onResponseSubmit(question);
+        // add event listener run again n again while we clicked the question button
+        // so we can't use add event listener here
+        // but we use modified add event listener like
+        // submitCommentNode.addEventListener('click', onResponseSubmit(question), { once: true })
+
+        //on upvote click
+        upvote.onclick = upvoteQuestion(question);
+        //on downvote click
+        downvote.onclick = downvoteQuestion(question);
     }
 }
 
 //listen for the submit response of the question open at the right panel
 function onResponseSubmit(question){
     return function(){
-        saveResponse(question);
+        var response = {
+            name :  commentatorNameNode.value,
+            description : commentNode.value 
+        }
+
+        saveResponse(question, response);
+        addResponseInPanel(response);
     }
 }
 
-//display response 
-function addResponseInPanel(){
-
-}
-
-//hide the details of the add question form
+// hide the details of the add question form
 function hideQuestionPanel(){
-    createQuestionFormNode.style.display = 'none';   
+    createQuestionFormNode.style.display = 'none';  
+} 
+// show the details of the question form
+function showQuestionPanel(){
+    createQuestionFormNode.style.display = 'block';   
 }
 
 // show the functions of the question on right side
@@ -144,11 +183,16 @@ function showDetailsOfQuestion(question){
     responseContainerNode.style.display = 'block';
     commentContainerNode.style.display = 'block';
 }
+// hide the functions of the question on right side
+function hideDetailsOfQuestion(question){
+    questionDetailsContainerNode.style.display = 'none';
+    resolveQuestionContainerNode.style.display = 'none';
+    responseContainerNode.style.display = 'none';
+    commentContainerNode.style.display = 'none';
+}
 
 // show the question data
 function addQuestionInRight(question){
-    // clear the previous question
-    questionDetailsContainerNode.innerHTML = '';
 
     var titleNode = document.createElement('h2');
     titleNode.innerHTML = question.title;
@@ -161,16 +205,13 @@ function addQuestionInRight(question){
 }
 
 // save the response to it's corresponding question in local storage
-function saveResponse(clickedQuestion){
+function saveResponse(clickedQuestion, response){
     var allQuestions = getAllQuestions();
 
     var revisedQuestions = allQuestions.map(function(question){
         if(clickedQuestion.title === question.title){
 
-            question.responses.push({
-                name :  commentatorNameNode.value,
-                description : commentNode.value 
-            })
+            question.responses.push(response)
         }
 
         return question;
@@ -180,4 +221,144 @@ function saveResponse(clickedQuestion){
     commentNode.value = '';
 
     localStorage.setItem('questions', JSON.stringify(revisedQuestions));
+}
+
+//display response 
+function addResponseInPanel(response){
+    var userNameNode = document.createElement('h3');
+    userNameNode.innerHTML = response.name;
+
+    var userCommentNode = document.createElement('p');
+    userCommentNode.innerHTML = response.description;
+
+    var container = document.createElement('div')
+    
+    container.appendChild(userNameNode)
+    container.appendChild(userCommentNode)
+
+    responseContainerNode.appendChild(container)
+}
+
+// clear the previous question
+function clearQuestionDetailes(){
+    questionDetailsContainerNode.innerHTML = '';
+}
+// clear the previous responses
+function clearResponsesDetailes(){
+    responseContainerNode.innerHTML = '';
+} 
+
+// search bar
+questionSearchNode.addEventListener('keyup', function(e){
+    // show filtered results
+    filterResult(e.target.value);
+})
+
+// filter for searching
+function filterResult(query){
+
+    var allQuestions = getAllQuestions();
+    if(query){ // if any value exist in search box then function search particular value present in our question or not
+        // clear all question
+        clearQuestionsPanel();
+
+        showfilteredResult(allQuestions, query)
+    }
+    else{ // if query box is empty then show all questions
+        clearQuestionsPanel()
+        allQuestions.forEach(function(question){
+            
+            addQuestionInPanel(question);
+        })
+    }
+
+}
+
+// show filtered results
+function showfilteredResult(allQuestions, query){
+    var filteredQuestions = allQuestions.filter(function(question){
+        if(question.title.includes(query)){
+            return true;
+        }
+    })
+    // same working 
+    // allQuestions.forEach(function(question){
+    //     if(question.title.includes(query)){
+    //         addQuestionInPanel(question);
+    //     }
+    // })
+    if(filteredQuestions.length){
+        filteredQuestions.forEach(function(question){
+            addQuestionInPanel(question);
+        })
+    }
+    else{
+        printNoMatchFound()
+    }
+}
+
+// cleared question panel after every search
+function clearQuestionsPanel(){
+    allQuestionsListNode.innerHTML = '';
+}
+
+// print no match found
+function printNoMatchFound(){
+    var title = document.createElement('h1');
+    title.innerHTML = 'No Match Found';
+
+    allQuestionsListNode.appendChild(title);
+}
+
+// upvotes question function
+function upvoteQuestion(question){
+    return function(){
+        question.upvotes++;
+        updateQuestion(question);
+        updateQuestionUI(question);
+    }
+}
+
+// downvotes question functions
+function downvoteQuestion(question){
+    return function(){
+        question.downvotes++;
+        updateQuestion(question);
+        updateQuestionUI(question);
+    }
+}
+
+// update question
+function updateQuestion(updatedQuestion){
+    var allQuestions = getAllQuestions();
+
+    var revisedQuestions = allQuestions.map(function(question){
+        if(updatedQuestion.title === question.title){
+
+            return updatedQuestion;
+        }
+
+        return question;
+    })
+
+    localStorage.setItem('questions', JSON.stringify(revisedQuestions));
+}
+
+function updateQuestionUI(question){
+
+    // get question container from DOM
+    var questionContainerNode = document.getElementById(question.title)
+
+    // console.log(questionContainerNode.childNodes)
+    questionContainerNode.childNodes[2].innerHTML = 'upvotes = '+ question.upvotes;
+    questionContainerNode.childNodes[3].innerHTML = 'downvotes = '+ question.downvotes;
+    
+}
+
+newQuestionFormBtn.addEventListener('click', openQuestionForm); // new question form btn
+
+// OPEN QUESTION FORM
+function openQuestionForm(){
+    hideDetailsOfQuestion();
+    showQuestionPanel();
 }
